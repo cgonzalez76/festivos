213f0c359e878d7090f825b6cceb7e132db78596
Carlos Gonzalez <cgonzalez@plusnetsolutions.com>
2017-06-08 17:31:00 +0200

    Cambios en bootstrap

--- WebContent/app/controllers/SpainController.js
+++ WebContent/app/controllers/SpainController.js
@@ -40,7 +40,7 @@ app.directive('wmBlock', function ($parse) {
 
 app.controller( "spainController", 
 
-    function($controller, $timeout, $scope, $rootScope, $location, $sce, $filter, md5, NotifyService, DTOptionsBuilder, DTColumnDefBuilder, adminServices, spainServices,tabsFactory) {
+    function($controller, $timeout, $scope, $rootScope, $location, $sce, $filter, md5, NotifyService, DTOptionsBuilder, DTColumnDefBuilder,spainServices,tabsFactory) {
 
 	$scope.comboCCAA = [];
 	$scope.comboProvincias = [];
